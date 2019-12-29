0b8d1bb1fc0e758253da963c4f694646ccdbef4c
Mara Petsani <marapetsani@gmail.com>
2019-12-29 18:57:45 +0200

    1st exercise

---
+++ exercises/modules/node/app.js
@@ -0,0 +1,30 @@
+//window.App = window.App || {}
+const {getUserById,getPostsForUser}=require("./api.js");
+
+showPostsForCurrentUser = (userId, cb) => {
+  getPostsForUser(userId, posts => {
+    const postTemplates = posts.map(post => {
+      return `
+      <div class="post">
+      ${post.title}
+      ${post.body}
+      ${post.createdBy}
+      </div>
+
+      `
+    })
+    cb(postTemplates)
+  })
+};
+
+showUserProfile = (userId, cb) => {
+ getUserById(userId, user => {
+    const profile = `
+    <div>
+    ${user.name}
+    </div>
+    `
+
+    cb(user)
+  })
+}
